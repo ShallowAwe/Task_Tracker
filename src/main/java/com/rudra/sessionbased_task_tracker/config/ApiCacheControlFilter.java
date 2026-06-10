3d185a72b3a296f8159a6e2bcbf0122563a6ceff
ShallowAwe <rudraindurkar670@gmail.com>
2026-06-10 09:28:48 +0530

    added a cache control filter nad updated the api's

---
+++ src/main/java/com/rudra/sessionbased_task_tracker/config/ApiCacheControlFilter.java
@@ -0,0 +1,32 @@
+package com.rudra.sessionbased_task_tracker.Config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class ApiCacheControlFilter extends OncePerRequestFilter {
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+        return !request.getRequestURI().startsWith("/api/");
+    }
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain) throws ServletException, IOException {
+
+        response.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0");
+        response.setHeader("Pragma", "no-cache");
+        response.setDateHeader("Expires", 0);
+
+        filterChain.doFilter(request, response);
+    }
+}
